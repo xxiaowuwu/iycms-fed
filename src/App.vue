ae6e2615854a7e688c4d5a06e3632dee92fd2377
あ读鈊茚ケ <839024615@qq.com>
2018-01-11 22:51:38 +0800

    no commit message

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+	<div id="app"  v-show="name">
+		<Header :nav="nav" ></Header>
+		<router-view @transferUser="abc"></router-view>
+		<Footer></Footer>
+	</div>
+</template> 
+<script>
+	export default {
+		name: 'app',
+		data() {
+			return {
+				name:true,
+				nav: [{
+						name: "关于我",
+						url: "/about",
+						
+						an: false,
+						list: [{
+								name: "个人资料",
+								url: "/article"
+							},
+							{
+								name: "专业技能",
+								url: "/article"
+							},
+							{
+								name: "项目经验",
+								url: "/article"
+							},
+							{
+								name: "工作经历",
+								url: "/article"
+							},
+						]
+					},
+
+					{
+						name: "实验室",
+						url: "/experiment"
+					},
+
+					{
+						name: "归档",
+						url: "/archive"
+					},
+					{
+						name: "博文",
+						url: "/article"
+					},
+					{
+						name: "首页",
+						url: "/",
+						active:true,
+					},
+				],
+			};
+		},
+		mounted: function(){
+		},
+		methods: {
+			abc: function(e){
+				console.log(e,"我是父级");
+			}
+		},
+		
+	}
+</script>
---
+++ src/controller/article.vue
@@ -0,0 +1,43 @@
+<template>
+	<el-container>
+		<div class="container">
+			<el-row>
+				<el-col :span="12">
+					<div class="grid-content bg-purple">2222222
+						<p><a @click="setabc">abdddd</a></p>
+						
+						2222222</div>
+				</el-col>
+				<el-col :span="12">
+					<div class="grid-content bg-purple-light">22222222222</div>
+				</el-col>
+			</el-row>
+		</div>
+	</el-container>
+</template>
+
+<script>
+	export default {
+		name: 'index',
+		data() {
+			return {
+				msg: 'Welcome to Your Vue.js App'
+			}
+		},
+		created(){
+			
+		},
+		methods: {
+			setabc: function(){
+				console.log(this.$route.path);
+				this.$emit("transferUser",123);
+			}
+			
+		}
+	}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
